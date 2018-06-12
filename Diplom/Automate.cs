433b4d07547e3ff5e6be8db44a17cd4edf1cc658
tosha4encko <toshapan4encko@yandex.ru>
2018-06-12 21:49:32 +0300

    Добавил кривые ребра

--- Diplom/Automate.cs
+++ Diplom/Automate.cs
@@ -68,9 +68,9 @@ namespace Diplom
                     Vertex v2 = new Vertex();
                     foreach (var v in list_v)
                     {
-                        if (v.Name == sub_str[1][0])
-                            v1 = v;
                         if (v.Name == sub_str[2][0])
+                            v1 = v;
+                        if (v.Name == sub_str[3][0])
                             v2 = v;
                     }
                     //Обработчик исключения
@@ -93,7 +93,7 @@ namespace Diplom
             list_v.Sort((v1, v2) => v2.count_e.CompareTo(v1.count_e));
 
             list_v[0].set_point(centr);
-            list_v[1].set_point(new Point(centr.X - 50, centr.Y - 50));
+            list_v[1].set_point(new Point(centr.X - 100, centr.Y - 100));
             int f = 0;
             int c_only = 1;
             int c_all = 2;
@@ -120,19 +120,19 @@ namespace Diplom
             if (f == 0)
             {
                 if (c_all / c_only >= 2)
-                    x0 += 100;
+                    x0 += 200;
 
                 if (c_all / c_only < 2)
-                    y0 += 100;
+                    y0 += 200;
             }
 
             if (f == 1)
             {
                 if (c_all / c_only >= 2)
-                    x0 -= 100;
+                    x0 -= 200;
 
                 if (c_all / c_only < 2)
-                    y0 -= 100;
+                    y0 -= 200;
             }
 
             return new Point(p.X + x0, p.Y + y0);
@@ -144,8 +144,75 @@ namespace Diplom
                 dh.DrawV(v.Name.ToString(), v.coordinate.X, v.coordinate.Y, v.is_end);
 
             foreach (var e in list_e)
-                dh.DrawE(e.from.coordinate.X+17, e.from.coordinate.Y + 17, 
-                         e.to.coordinate.X + 17, e.to.coordinate.Y + 17);
+            {
+                if (e.from == e.to)
+                {
+                    dh.DrawE(e.from.coordinate.X + 17, e.from.coordinate.Y + 36,
+                    e.from.coordinate.X + 17 - 50, e.from.coordinate.Y + 17 + 50,
+                    e.to.coordinate.X + 17 + 50, e.to.coordinate.Y + 17 + 50,
+                    e.to.coordinate.X + 17, e.to.coordinate.Y + 36, e.Name);
+                }
+                else
+                {
+                    int x1 = e.from.coordinate.X;
+                    int y1 = e.from.coordinate.Y;
+                    int x2 = e.to.coordinate.X;
+                    int y2 = e.to.coordinate.Y;
+
+                    bool p_intersection = false;
+                    int xc = 0;
+                    int yc = 0;
+                    foreach (var v in list_v)
+                    {
+                        if (v != e.from && v != e.to)
+                        {
+                            double H = getH(x1, y1, x2, y2, v.coordinate.X, v.coordinate.Y);
+                            if (H < 40 && H != -1)
+                            {
+                                p_intersection = true;
+
+                                int cx1 = Math.Sign(v.coordinate.X - x1);
+                                int cx2 = Math.Sign(v.coordinate.X - x2);
+                                xc = cx1 * cx2 * 50;
+
+                                int cy1 = Math.Sign(v.coordinate.Y - y1);
+                                int cy2 = Math.Sign(v.coordinate.Y - y2);
+                                yc = cy1 * cy2 * 50;
+
+                                break;
+                            }
+                        }
+                    }
+
+                    if (p_intersection)
+                    {
+                        int xw = (e.from.coordinate.X + 17 + e.to.coordinate.X + 17) / 2 + xc;
+                        int yw = (e.from.coordinate.Y + 17 + e.to.coordinate.Y + 17) / 2 + yc;
+                        dh.DrawE(e.from.coordinate.X + 17, e.from.coordinate.Y + 17, xw, yw,
+                                   e.to.coordinate.X + 17, e.to.coordinate.Y + 17, e.Name);
+                    }
+                    else
+                        dh.DrawE(e.from.coordinate.X + 17, e.from.coordinate.Y + 17,
+                                 e.to.coordinate.X + 17, e.to.coordinate.Y + 17, e.Name);
+                }
+            }
+        }
+
+        private double getH(int x1, int y1, int x2, int y2, int vx, int vy)
+        {
+            double a = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+            double b = Math.Sqrt(Math.Pow(vx - x2, 2) + Math.Pow(vy - y2, 2));
+            double c = Math.Sqrt(Math.Pow(x1 - vx, 2) + Math.Pow(y1 - vy, 2));
+
+            if (a < c || a < b)
+                return -1;
+
+            double p = (a+b+c) / 2;
+            double S = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+
+            double h = S * 2 / a;
+
+            return h;
         }
     }
 }
--- Diplom/DrawAutomate.cs
+++ Diplom/DrawAutomate.cs
@@ -20,28 +20,34 @@ namespace Diplom
            
         }
 
-        public void DrawE(int x1, int y1, int sx1, int sy1, int sx2, int sy2, int x2, int y2)
+        public void DrawE(int x1, int y1, int sx1, int sy1, int sx2, int sy2, int x2, int y2, string name)
         {
             html_e += String.Format("<path d =\"M {0}, {1} C {2}, {3}, {4}, {5}, {6}, {7}\"  stroke=\"black\" fill=\"transparent\" />",
-                x1.ToString(), y1.ToString(), sx1.ToString(), sy1.ToString(), 
-                sx2.ToString(), sy2.ToString(), x2.ToString(), y2.ToString());
+                x1.ToString(), y1.ToString(), 
+                sx1.ToString(), sy1.ToString(), 
+                sx2.ToString(), sy2.ToString(), 
+                x2.ToString(), y2.ToString());
 
             html_e += String.Format("<circle cx=\"{0}\" cy=\"{1}\" r=\"2\" stroke-width=\"1\" stroke=\"black\"/>",
                 x2.ToString(), y2.ToString());
 
+            DrawNameV(name, (sx1 + sx2 - 34) / 2, (sy1 + sy2 - 34) / 2); // 17+17 == 34
+
         }
 
-        public void DrawE(int x1, int y1, int sx1, int sy1, int x2, int y2)
+        public void DrawE(int x1, int y1, int sx1, int sy1, int x2, int y2, string name)
         {
-            html_e += String.Format("<path d =\"M10 {0} {1} Q {2} {3} {4} {5}\" stroke=\"black\" fill=\"transparent\" />",
+            html_e += String.Format("<path d =\"M {0} {1} Q {2} {3} {4} {5}\" stroke=\"black\" fill=\"transparent\" />",
                 x1.ToString(), y1.ToString(), sx1.ToString(), sy1.ToString(), x2.ToString(), y2.ToString());
 
             html_e += String.Format("<circle cx=\"{0}\" cy=\"{1}\" r=\"2\" stroke-width=\"1\" stroke=\"black\"/>",
                 x2.ToString(), y2.ToString());
 
+            DrawNameV(name, sx1, sy1); // 17+17 == 34
+
         }
 
-        public void DrawE(int x1, int y1, int x2, int y2)
+        public void DrawE(int x1, int y1, int x2, int y2, string name)
         {
             html_e += String.Format("<line stroke-width=\"1\" stroke=\"black\" x1 = \"{0}\"  y1 = \"{1}\" x2 = \"{2}\" y2 = \"{3}\"/>",
                 x1.ToString(), y1.ToString(), x2.ToString(), y2.ToString());
@@ -49,6 +55,7 @@ namespace Diplom
             html_e += String.Format("<circle cx=\"{0}\" cy=\"{1}\" r=\"2\" stroke-width=\"1\" stroke=\"black\"/>",
                 x2.ToString(), y2.ToString());
 
+            DrawNameV(name, (x1 + x2 - 34) / 2, (y1 + y2 - 34) / 2); // 17+17 == 34
         }
 
         public void DrawV(string name, int left, int top, int is_end)
@@ -68,6 +75,18 @@ namespace Diplom
 
             countV += 1;
         }
+
+        public void DrawNameV(string name, int left, int top)
+        {
+            html_style += String.Format(" .e{0}", name[0]) + "{ "
+                        + String.Format(" padding: 13px 0 0 20px; width: 30px; height: 30px; position: absolute; color: #800080; font-size: 25px;" +
+                                                "top:  {0}px; left:  {1}px;",
+                                                top.ToString(), left.ToString()) + "}";
+            html_v += String.Format(" <div class=\"e{0}\">{0}</div> ", name[0]);
+
+            countV += 1;
+        }
+
 
     }
 }
